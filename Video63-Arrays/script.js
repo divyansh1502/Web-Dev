8da0fa62dd820b449b4e7d2a2d5ecf9a1d6707bd
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-09-27 22:13:54 +0530

    Arrays Half Lec

---
+++ Video63-Arrays/script.js
@@ -0,0 +1,28 @@
+let arr = [1, 2, 4, 5, 7];
+let arr2 = ["Mango", "Grapes", "Ajay"];
+console.log(arr);
+console.log(arr.length)
+arr[0] = 55;
+console.log(arr, typeof arr);
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+
+console.log(arr.toString());
+console.log(arr.join(" and "));
+
+arr.pop();
+console.log(arr);
+arr.push(100);
+arr.push("Thakur Sahab");
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift("Divyansh");
+console.log(arr);
+delete arr[5];
+console.log(arr);
+let a = arr.concat(arr2);
+console.log(a);
