ce96903c05e3ac18b554180b52a64b0011d895b8
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2025-10-05 14:59:04 +0530

    JS Promise and Callback

---
+++ Video75-Callback & Promises/script.js
@@ -0,0 +1,18 @@
+// console.log("Divyansh singh is a Hacker");
+// console.log("Rohan is a Hacker");
+
+// setTimeout(() => {
+//     console.log("I am inside timeOut 1");
+// },1000)
+// setTimeout(() => {
+//     console.log("I am inside timeOut 2");
+// },0)
+// console.log("The end");
+
+// const loadScript = (src, callback)=>{
+//     let sc = document.createElement("script");
+//     sc.src = src;
+//     sc.onload = callback("Divyansh");
+//     document.head.append(sc);
+// }
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback);
